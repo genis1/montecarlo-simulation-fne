2fb38532952831d7739521b9d2688eddad268748
Genís Guillem Mumó <genisguillem@gmail.com>
2024-12-07 10:18:28 +0100

    Small changes.

--- simulation.py
+++ simulation.py
@@ -15,17 +15,21 @@ MACROSCOPIC_CROSS_SECTION = 0.361663
 def get_distance_to_next_interaction(macroscopic_cross_section):
     return - math.log(random()) / macroscopic_cross_section
 
+
 def get_atomic_mass_target():
     if random() < HYDROGEN_SCATTERING_PERCENTAGE:
         return 1
     else:
         return 16
 
+
 def is_outside_right(position):
     return position[0] > 30
 
+
 def is_outside_left(position):
     return position[0] < 0
+
 
 def is_absorbed():
     return random() < ABSORBANCE_PERCENTAGE
@@ -33,6 +37,7 @@ def is_absorbed():
 
 def is_thermalized(v_f):
     return get_norm(v_f) < THERMALIZED_VELOCITY_THRESHOLD
+
 
 def simulate(x, v, ENERGY_ANALYSED_COLLISIONS):
     positions = []
@@ -46,22 +51,20 @@ def simulate(x, v, ENERGY_ANALYSED_COLLISIONS):
         x = x + ((distance / get_norm(v)) * v)
 
         if is_outside_right(x):
-            return "ESCAPED_RIGHT", positions, scattering_angles, energies
+            return "ESCAPED_RIGHT", positions, scattering_angles, energies, num_collisions
         if is_outside_left(x):
-            return "ESCAPED_LEFT", positions, scattering_angles, energies
+            return "ESCAPED_LEFT", positions, scattering_angles, energies, num_collisions
         if is_absorbed():
-            return "ABSORBED", positions, scattering_angles, energies
+            return "ABSORBED", positions, scattering_angles, energies, num_collisions
 
         v, theta_lab, energy = elastic_collision(v, get_atomic_mass_target())
         scattering_angles.append(theta_lab)
 
         if num_collisions < ENERGY_ANALYSED_COLLISIONS:
             energies.append(energy)
-
+        num_collisions += 1
         if is_thermalized(v):
             positions.append(x)
-            return "THERMALIZED", positions, scattering_angles, energies
+            return "THERMALIZED", positions, scattering_angles, energies, num_collisions
 
         positions.append(x)
-
-        num_collisions += 1
