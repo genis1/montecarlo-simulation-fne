cc60f54d6f45a0d26ef4017bcc861d8245e0d65d
Genís Guillem Mumó <genisguillem@gmail.com>
2024-12-07 19:11:17 +0100

    Created total flux for two region slab.

---
+++ results_two_slab.py
@@ -0,0 +1,48 @@
+import math
+from collections import defaultdict
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from simulation_two_slab import simulate_simple_two_slab, get_media, \
+    get_macroscopic_cross_section_absorbance
+
+
+def plot_flux_over_position(num_simulations, num_buckets, water_width):
+    bucket_width = 30 / num_buckets  # cm
+    bucket_counts = defaultdict(int)
+    initial_neutron_flux = 1000  # n/cm^2 s
+
+    initial_position = np.array([0, 0, 0])
+    initial_velocity = np.array([1, 0, 0])
+
+    for i in range(num_simulations):
+        result, final_position = simulate_simple_two_slab(initial_position, initial_velocity, water_width)
+        if result != "ABSORBED":
+            continue
+
+        final_x = final_position[0]
+
+        bucket_number = math.floor(final_x / bucket_width)
+        bucket_counts[bucket_number] += 1
+
+    def transform(position, count):
+        cs_absorbance = get_macroscopic_cross_section_absorbance(get_media([position, 0, 0], water_width))
+        return count * initial_neutron_flux / (
+                num_simulations * bucket_width * cs_absorbance)
+
+    flux_dict = {bucket * bucket_width: transform(bucket * bucket_width, count) for bucket, count in
+                 bucket_counts.items()}
+    flux = np.array(list(flux_dict.values()))
+    positions = np.array(list(flux_dict.keys()))
+
+    plt.bar(positions, flux, color='skyblue', edgecolor='black', width=bucket_width)
+    plt.xlabel('Distance (cm)')
+    plt.ylabel('Flux ($cm^{-2}s^{-1}$)')
+    plt.title('Flux over position for an initial flux of 1000$cm^{-2}s^{-1}$')
+    plt.show()
+
+
+plot_flux_over_position(num_simulations=200000, num_buckets=60, water_width=5)
+plot_flux_over_position(num_simulations=200000, num_buckets=60, water_width=10)
+plot_flux_over_position(num_simulations=200000, num_buckets=60, water_width=15)
--- simulation_two_slab.py
+++ simulation_two_slab.py
@@ -5,23 +5,101 @@ from numpy.linalg import norm as get_norm
 
 from main import elastic_collision
 
+MACROSCOPIC_CS_ABSORPTION_WATER = 0.010063
+MACROSCOPIC_CS_ABSORPTION_CARBON = 0.00026
+
 HYDROGEN_SCATTERING_PERCENTAGE = 7.8 / 10.5
-ABSORBANCE_PERCENTAGE = 0.010063 / 0.361663
+ABSORBANCE_RATIO_WATER = MACROSCOPIC_CS_ABSORPTION_WATER / 0.361663
+ABSORBANCE_RATIO_CARBON = MACROSCOPIC_CS_ABSORPTION_CARBON / (0.3846 + MACROSCOPIC_CS_ABSORPTION_CARBON)
 # Unitless, initial velocity 1MeV, thermalized velocity threshold 1eV
 THERMALIZED_VELOCITY_THRESHOLD = 1 / math.sqrt(10 ** 6)
 # In cm-1
-MACROSCOPIC_CROSS_SECTION = 0.361663
+MACROSCOPIC_CROSS_SECTION_WATER = 0.361663
+MACROSCOPIC_CROSS_SECTION_CARBON = 0.3846 + MACROSCOPIC_CS_ABSORPTION_CARBON  # cm-1
 
 
-def get_distance_to_next_interaction(macroscopic_cross_section):
-    return - math.log(random()) / macroscopic_cross_section
-
-
-def get_atomic_mass_target():
-    if random() < HYDROGEN_SCATTERING_PERCENTAGE:
-        return 1
+def get_media(x, water_width):
+    position = x[0]
+    if position < 0 or position > 30:
+        return "VOID"
+    elif position < water_width:
+        return "WATER"
     else:
-        return 16
+        return "CARBON"
+
+
+def get_macroscopic_cross_section(current_media):
+    if current_media == "WATER":
+        return MACROSCOPIC_CROSS_SECTION_WATER
+    else:
+        # Using the same cross-section for carbon and void, but it is not relevant.
+        return MACROSCOPIC_CROSS_SECTION_CARBON
+
+
+def get_macroscopic_cross_section_absorbance(current_media):
+    if current_media == "WATER":
+        return MACROSCOPIC_CS_ABSORPTION_WATER
+    else:
+        # Using the same cross-section for carbon and void, but it is not relevant.
+        return MACROSCOPIC_CS_ABSORPTION_CARBON
+
+
+def get_other_macroscopic_cross_section(current_media):
+    if current_media == "WATER":
+        return MACROSCOPIC_CROSS_SECTION_CARBON
+    else:
+        return MACROSCOPIC_CROSS_SECTION_WATER
+
+
+def get_next_position(current_position, v, water_width):
+    current_media = get_media(current_position, water_width)
+    macroscopic_cross_section = get_macroscopic_cross_section(current_media)
+    minus_log_r = - math.log(random())
+    distance_to_next_interaction = minus_log_r / macroscopic_cross_section
+    next_position = current_position + ((distance_to_next_interaction / get_norm(v)) * v)
+
+    next_media = get_media(next_position, water_width)
+
+    if next_media == current_media:
+        return next_position
+    if v[0] > 0 and current_position[0] > water_width:  # Escaped right
+        return next_position
+    if v[0] < 0 and current_position[0] < water_width:  # Escaped left
+        return next_position
+    else:  # Media change from water to graphite or the other way around.
+        next_macroscopic_cs = get_other_macroscopic_cross_section(current_media)
+
+        position_change_medium = current_position + v * ((water_width - current_position[0]) / v[0])
+        # print("####################### Start #####################")
+        # print(f"Current position is : {current_position}")
+        # print(f"Previous next position is : {next_position}")
+        # print(f"The position_change_medium is : {position_change_medium}")
+        # print(f"Velocity is : {v}")
+
+        d1 = get_norm(position_change_medium - current_position)
+        # print(f"d1 : {d1}")
+        d2 = (minus_log_r - d1 * macroscopic_cross_section) / next_macroscopic_cs
+        # print(f"d2 : {d2}")
+        dt = d1 + d2
+        # print(f"dt : {dt}")
+        # print(f"Previous dt : {distance_to_next_interaction}")
+
+        next_position = current_position + ((dt / get_norm(v)) * v)
+        # print(f"Recalculated next position is : {next_position}")
+
+        return next_position
+
+
+def get_atomic_mass_target(x, water_width):
+    media = get_media(x, water_width)
+
+    if media == "WATER":
+        if random() < HYDROGEN_SCATTERING_PERCENTAGE:
+            return 1
+        else:
+            return 16
+    elif media == "CARBON":
+        return 12
 
 
 def is_outside_right(position):
@@ -32,30 +110,30 @@ def is_outside_left(position):
     return position[0] < 0
 
 
-def is_absorbed():
-    return random() < ABSORBANCE_PERCENTAGE
+def is_absorbed(x, water_width):
+    media = get_media(x, water_width)
+    if media == "WATER":
+        return random() < ABSORBANCE_RATIO_WATER
+    elif media == "CARBON":
+        return random() < ABSORBANCE_RATIO_CARBON
 
 
 def is_thermalized(v_f):
     return get_norm(v_f) < THERMALIZED_VELOCITY_THRESHOLD
 
 
-def simulate_simple(x, v):
+def simulate_simple_two_slab(x, v, water_width):
     while True:
-        distance = get_distance_to_next_interaction(MACROSCOPIC_CROSS_SECTION)
-        x = x + ((distance / get_norm(v)) * v)
+        x = get_next_position(x, v, water_width)
 
         if is_outside_right(x):
             return "ESCAPED_RIGHT", x
         if is_outside_left(x):
             return "ESCAPED_LEFT", x
-        if is_absorbed():
+        if is_absorbed(x, water_width):
             return "ABSORBED", x
 
-        v, theta_lab, energy = elastic_collision(v, get_atomic_mass_target())
+        v, theta_lab, energy = elastic_collision(v, get_atomic_mass_target(x, water_width))
 
         if is_thermalized(v):
             return "THERMALIZED", x
-
-
-
