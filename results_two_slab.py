7cb85c9689d4a8216f8e5c1314fa0bf86b1baef0
Genís Guillem Mumó <genisguillem@gmail.com>
2024-12-17 20:54:45 +0100

    Converted histogram plots to scatter plots

--- results_two_slab.py
+++ results_two_slab.py
@@ -10,7 +10,7 @@ from simulation_two_slab import simulate_simple_two_slab, get_media, \
     get_macroscopic_cross_section_scattering
 
 
-def plot_flux_over_position(num_simulations, num_buckets, water_width):
+def plot_flux_over_position(num_simulations, num_buckets, water_width, color):
     bucket_width = 30 / num_buckets  # cm
     bucket_counts = defaultdict(int)
     initial_neutron_flux = 1000  # n/cm^2 s
@@ -42,17 +42,29 @@ def plot_flux_over_position(num_simulations, num_buckets, water_width):
     flux = np.array(list(flux_dict.values()))
     positions = np.array(list(flux_dict.keys()))
 
-    plt.plot(positions, flux, color='skyblue', linestyle='-', linewidth=2)
+    plt.plot(positions, flux, color=color, linestyle='-', linewidth=2, label=f"Water {water_width}cm")
     plt.xlabel('Distance (cm)')
     plt.ylabel('Flux ($cm^{-2}s^{-1}$)')
     plt.title('Flux over position for an initial flux of 1000$cm^{-2}s^{-1}$')
-    plt.axvline(x=water_width, color='red', linestyle='--', linewidth=1.5)
-    file_name = f"total_flux_{water_width}cm_{num_simulations}_num_buckets{num_buckets}"
+    plt.axvline(x=water_width, color=color, linestyle='--', linewidth=0.5)
+    print("Simulation done")
+
+
+def plot_flux_over_position_times_4(num_simulations_1, num_buckets_1):
+    plot_flux_over_position(num_simulations=num_simulations_1, num_buckets=num_buckets_1, water_width=30,
+                            color='skyblue')
+    plot_flux_over_position(num_simulations=num_simulations_1, num_buckets=num_buckets_1, water_width=5, color='brown')
+    plot_flux_over_position(num_simulations=num_simulations_1, num_buckets=num_buckets_1, water_width=10,
+                            color='olive')
+    plot_flux_over_position(num_simulations=num_simulations_1, num_buckets=num_buckets_1, water_width=15,
+                            color='limegreen')
+    plt.legend()
+    file_name = f"total_flux_{num_simulations_1}_num_buckets{num_buckets_1}"
     plt.savefig(f"figures/{file_name}.png", dpi=300, bbox_inches='tight')
     plt.clf()
 
 
-def plot_single_collision_flux(num_simulations, num_buckets, water_width):
+def plot_single_collision_flux(num_simulations, num_buckets, water_width, color):
     bucket_width = 30 / num_buckets  # cm
     bucket_counts = defaultdict(int)
     initial_neutron_flux = 1000  # n/cm^2 s
@@ -84,17 +96,29 @@ def plot_single_collision_flux(num_simulations, num_buckets, water_width):
     flux = np.array(list(flux_dict.values()))
     positions = np.array(list(flux_dict.keys()))
 
-    plt.plot(positions, flux, color='skyblue', linestyle='-', linewidth=2)
+    plt.plot(positions, flux, color=color, linestyle='-', linewidth=0.5, label=f"Water {water_width}cm")
     plt.xlabel('Distance (cm)')
     plt.ylabel('Flux ($cm^{-2}s^{-1}$)')
     plt.title('Flux over position for an initial flux of 1000$cm^{-2}s^{-1}$')
-    plt.axvline(x=water_width, color='red', linestyle='--', linewidth=1.5)
-    file_name = f"single_collision_flux_{water_width}cm_{num_simulations}_num_buckets{num_buckets}"
+    plt.axvline(x=water_width, color=color, linestyle='--', linewidth=0.5)
+    print("Simulation done")
+
+
+def plot_single_collision_flux_times_4(num_simulations, num_buckets):
+    plot_single_collision_flux(num_simulations=num_simulations, num_buckets=num_buckets, water_width=30,
+                               color='skyblue')
+    plot_single_collision_flux(num_simulations=num_simulations, num_buckets=num_buckets, water_width=5, color='brown')
+    plot_single_collision_flux(num_simulations=num_simulations, num_buckets=num_buckets, water_width=10,
+                               color='olive')
+    plot_single_collision_flux(num_simulations=num_simulations, num_buckets=num_buckets, water_width=15,
+                               color='limegreen')
+    plt.legend()
+    file_name = f"single_collision_flux_{num_simulations}_num_buckets{num_buckets}"
     plt.savefig(f"figures/{file_name}.png", dpi=300, bbox_inches='tight')
     plt.clf()
 
 
-def plot_multiple_collision_flux_over_position(num_simulations, num_buckets, water_width):
+def plot_multiple_collision_flux_over_position(num_simulations, num_buckets, water_width, color):
     bucket_width = 30 / num_buckets  # cm
     bucket_counts = defaultdict(int)
     initial_neutron_flux = 1000  # n/cm^2 s
@@ -127,17 +151,30 @@ def plot_multiple_collision_flux_over_position(num_simulations, num_buckets, wat
     flux = np.array(list(flux_dict.values()))
     positions = np.array(list(flux_dict.keys()))
 
-    plt.plot(positions, flux, color='skyblue', linestyle='-', linewidth=2)
+    plt.plot(positions, flux, color=color, linestyle='-', linewidth=2, label=f"Water {water_width}cm")
     plt.xlabel('Distance (cm)')
     plt.ylabel('Flux ($cm^{-2}s^{-1}$)')
     plt.title('Flux over position for an initial flux of 1000$cm^{-2}s^{-1}$')
-    plt.axvline(x=water_width, color='red', linestyle='--', linewidth=1.5)
-    file_name = f"multiple_collision_flux_{water_width}cm_{num_simulations}_num_buckets{num_buckets}"
+    plt.axvline(x=water_width, color=color, linestyle='--', linewidth=0.5)
+    print("Simulation done")
+
+
+def plot_multiple_collision_flux_over_position_times_4(num_simulations, num_buckets):
+    plot_multiple_collision_flux_over_position(num_simulations=num_simulations, num_buckets=num_buckets, water_width=30,
+                                               color='skyblue')
+    plot_multiple_collision_flux_over_position(num_simulations=num_simulations, num_buckets=num_buckets, water_width=5,
+                                               color='brown')
+    plot_multiple_collision_flux_over_position(num_simulations=num_simulations, num_buckets=num_buckets, water_width=10,
+                                               color='olive')
+    plot_multiple_collision_flux_over_position(num_simulations=num_simulations, num_buckets=num_buckets, water_width=15,
+                                               color='limegreen')
+    plt.legend()
+    file_name = f"multiple_collision_flux_{num_simulations}_num_buckets{num_buckets}"
     plt.savefig(f"figures/{file_name}.png", dpi=300, bbox_inches='tight')
     plt.clf()
 
 
-def plot_slowing_down_density(num_simulations, num_buckets, water_width):
+def plot_slowing_down_density(num_simulations, num_buckets, water_width, color):
     bucket_width = 30 / num_buckets  # cm
     bucket_counts = defaultdict(int)
     initial_neutron_flux = 1000  # n/cm^2 s
@@ -166,29 +203,40 @@ def plot_slowing_down_density(num_simulations, num_buckets, water_width):
     flux = np.array(list(flux_dict.values()))
     positions = np.array(list(flux_dict.keys()))
 
-    plt.plot(positions, flux, color='skyblue', linestyle='-', linewidth=2)
+    plt.plot(positions, flux, color=color, linestyle='-', linewidth=2, label=f"Water {water_width}cm")
     plt.xlabel('Distance (cm)')
     plt.ylabel('Slowing down density ($cm^{-3}s^{-1}$)')
     plt.title('Slowing down density over position for an initial flux of 1000$cm^{-2}s^{-1}$')
-    plt.axvline(x=water_width, color='red', linestyle='--', linewidth=1.5)
-    file_name = f"slowing_down_density_{water_width}cm_{num_simulations}_num_buckets{num_buckets}"
+    plt.axvline(x=water_width, color=color, linestyle='--', linewidth=0.5)
+    print("Simulation done")
+
+
+
+def plot_slowing_down_density_times_4(num_simulations, num_buckets):
+    plot_slowing_down_density(num_simulations=num_simulations, num_buckets=num_buckets, water_width=30,
+                              color='skyblue')
+    plot_slowing_down_density(num_simulations=num_simulations, num_buckets=num_buckets, water_width=5, color='brown')
+    plot_slowing_down_density(num_simulations=num_simulations, num_buckets=num_buckets, water_width=10,
+                              color='olive')
+    plot_slowing_down_density(num_simulations=num_simulations, num_buckets=num_buckets, water_width=15,
+                              color='limegreen')
+    plt.legend()
+    file_name = f"slowing_down_density_{num_simulations}_num_buckets{num_buckets}"
     plt.savefig(f"figures/{file_name}.png", dpi=300, bbox_inches='tight')
     plt.clf()
 
 
-scale_num_simulation = 10  # 100 for standard, 1000 for good results, 1 for quick test
+scale_num_simulation = 1  # 100 for standard, 1000 for good results, 1 for quick test
 os.makedirs("figures", exist_ok=True)
-plot_flux_over_position(num_simulations=2000 * scale_num_simulation, num_buckets=60, water_width=5)
-#plot_flux_over_position(num_simulations=2000 * scale_num_simulation, num_buckets=60, water_width=10)
-#plot_flux_over_position(num_simulations=2000 * scale_num_simulation, num_buckets=60, water_width=15)
-plot_single_collision_flux(num_simulations=50000 * scale_num_simulation, num_buckets=600, water_width=5)
-#plot_single_collision_flux(num_simulations=50000 * scale_num_simulation, num_buckets=600, water_width=10)
-#plot_single_collision_flux(num_simulations=50000 * scale_num_simulation, num_buckets=600, water_width=15)
-plot_multiple_collision_flux_over_position(num_simulations=2000 * scale_num_simulation, num_buckets=60, water_width=5)
-#plot_multiple_collision_flux_over_position(num_simulations=2000 * scale_num_simulation, num_buckets=60,
-#                                           water_width=10)
-#plot_multiple_collision_flux_over_position(num_simulations=2000 * scale_num_simulation, num_buckets=60,
-#                                           water_width=15)
-plot_slowing_down_density(num_simulations=2000 * scale_num_simulation, num_buckets=60, water_width=5)
-#plot_slowing_down_density(num_simulations=2000 * scale_num_simulation, num_buckets=60, water_width=10)
-#plot_slowing_down_density(num_simulations=2000 * scale_num_simulation, num_buckets=60, water_width=15)
+
+plot_flux_over_position_times_4(2000 * scale_num_simulation, 60)
+print("Figure 1")
+
+plot_single_collision_flux_times_4(num_simulations=50000 * scale_num_simulation, num_buckets=600)
+print("Figure 2")
+
+plot_multiple_collision_flux_over_position_times_4(num_simulations=2000 * scale_num_simulation, num_buckets=60)
+print("Figure 3")
+
+plot_slowing_down_density_times_4(num_simulations=2000 * scale_num_simulation, num_buckets=60)
+print("Figure 4")
